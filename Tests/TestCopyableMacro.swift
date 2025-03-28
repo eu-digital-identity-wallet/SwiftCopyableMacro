7302c8325e2b08afa6187b77ccefd0b208dbde92
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2025-03-28 04:55:09 +0200

    Finalized all refinements and restructuring.

---
+++ Tests/TestCopyableMacro.swift
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European
+ * Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work
+ * except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific language
+ * governing permissions and limitations under the Licence.
+ */
+import XCTest
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+
+#if canImport(CopyablePlugin)
+@testable import CopyablePlugin
+
+let testMacros: [String: Macro.Type] = [
+  "Copyable": CopyableMacro.self
+]
+#endif
+
+final class TestCopyableMacro: XCTestCase {
+  
+  func testExpansion_WhenVisibilityIsInternalAndNoOptionals_ThenAssertResult() throws {
+#if canImport(CopyablePlugin)
+    assertMacroExpansion(
+            """
+            @Copyable
+            struct User {
+                let name: String
+                let age: Int
+            }
+            """,
+            expandedSource: """
+            struct User {
+                let name: String
+                let age: Int
+            
+                /// Returns a copy of the caller whose value for `name` is different.
+                internal func copy(name: String) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose value for `age` is different.
+                internal func copy(age: Int) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose values are different. All values are optional and the previous value will be used if not set.
+                internal func copy(name: String? = nil, age: Int? = nil) -> Self {
+                    .init(name: name ?? self.name, age: age ?? self.age)
+                }
+            }
+            """,
+            macros: testMacros
+    )
+#else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+#endif
+  }
+  
+  func testExpansion_WhenVisibilityIsPublicAndNoOptionals_ThenAssertResult() throws {
+#if canImport(CopyablePlugin)
+    assertMacroExpansion(
+            """
+            @Copyable
+            public struct User {
+                public let name: String
+                public let age: Int
+            }
+            """,
+            expandedSource: """
+            public struct User {
+                public let name: String
+                public let age: Int
+            
+                /// Returns a copy of the caller whose value for `name` is different.
+                public func copy(name: String) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose value for `age` is different.
+                public func copy(age: Int) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose values are different. All values are optional and the previous value will be used if not set.
+                public func copy(name: String? = nil, age: Int? = nil) -> Self {
+                    .init(name: name ?? self.name, age: age ?? self.age)
+                }
+            }
+            """,
+            macros: testMacros
+    )
+#else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+#endif
+  }
+  
+  func testExpansion_WhenVisibilityIsInternalWithOptionals_ThenAssertResult() throws {
+#if canImport(CopyablePlugin)
+    assertMacroExpansion(
+            """
+            @Copyable
+            struct User {
+                let name: String
+                let age: Int?
+            }
+            """,
+            expandedSource: """
+            struct User {
+                let name: String
+                let age: Int?
+            
+                /// Returns a copy of the caller whose value for `name` is different.
+                internal func copy(name: String) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose value for `age` is different.
+                internal func copy(age: Int?) -> Self {
+                    .init(name: name, age: age)
+                }
+            
+                /// Returns a copy of the caller whose values are different. All values are optional and the previous value will be used if not set.
+                internal func copy(name: String? = nil, age: Int? = nil) -> Self {
+                    .init(name: name ?? self.name, age: age ?? self.age)
+                }
+            }
+            """,
+            macros: testMacros
+    )
+#else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+#endif
+  }
+}
